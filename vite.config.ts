a6ba15bdefd73911395f06db68e637568ed633be
Stanislav <stanislavstranger@gmail.com>
2024-08-02 18:18:19 +0300

    fix: fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_URL,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL,
+  };
 });
+
